5dd16417b3d990eca0df8b00506fae7a449ebb50
Masaki Toyoshima <masaki.toyoshima@sheep-medical.com>
2022-10-11 18:31:29 +0900

    1st commit

---
+++ ut/fizzbuzz.go
@@ -0,0 +1,5 @@
+package main
+
+func FizzBuzz(nums []int) string {
+	return "hello"
+}
---
+++ ut/fizzbuzz_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test1",
+			args: args{
+				nums: []int{1, 2, 3},
+			},
+			want: "hello",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FizzBuzz(tt.args.nums); got != tt.want {
+				t.Errorf("FizzBuzz() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
